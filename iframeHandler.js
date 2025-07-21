8e8758deb5ff7b7ae0519884d0f380d7c9fe582e
WolfieBabe <110204635+WolfieBabe@users.noreply.github.com>
2025-07-20 23:25:22 -0500

    Update iframeHandler.js

--- iframeHandler.js
+++ iframeHandler.js
@@ -1,29 +1,21 @@
 let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
-var iframes = Array.from(document.querySelectorAll('iframe'));
+const iframes = Array.from(document.querySelectorAll('iframe'));
 const pdfContainers = document.querySelectorAll('.pdfobject-container');
 const matches = [];
 
-for (const pdfContainer of pdfContainers) {for (let i = iframes.length - 1; i >= 0; i--) {
-    const iframe = iframes[i];
-    if (pdfContainer.contains(iframe)) {
-        matches.push({ container, iframe });
-        iframes.splice(i, 1)
+for (const pdfContainer of pdfContainers) {
+    for (let i = iframes.length - 1; i >= 0; i--) {
+        const iframe = iframes[i];
+        if (pdfContainer.contains(iframe)) {
+            matches.push({ container: pdfContainer, iframe });
+            iframes.splice(i, 1);
+        }
     }
-}  
+}
     
 iframes.forEach(iframe => {
-    if (vw >= vh) {
-        iframe.width = vw / 2;
-        iframe.height = vh * .8;   
-    } else {
-        iframe.height = vw / 2;
-        iframe.width = vh * .8;
-    }
-})
-
-pdfContainers.forEach(iframe => {
     if (vw >= vh) {
         iframe.width = vw / 2;
         iframe.height = vh * .8;   
