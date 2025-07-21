d4bd53dd9795be91ca38c533b76be6e934416172
WolfieBabe <110204635+WolfieBabe@users.noreply.github.com>
2025-07-21 01:58:56 -0500

    Update iframeHandler.js

--- iframeHandler.js
+++ iframeHandler.js
@@ -1,20 +1,8 @@
 let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
-const iframes = Array.from(document.querySelectorAll('iframe'));
-const pdfContainers = document.querySelectorAll('.pdfobject-container');
-const matches = [];
+const iframes = Array.from(document.querySelectorAll('iframe', '.pdfobject-container'));
 
-for (const pdfContainer of pdfContainers) {
-    for (let i = iframes.length - 1; i >= 0; i--) {
-        const iframe = iframes[i];
-        if (pdfContainer.contains(iframe)) {
-            matches.push({ pdfContainer, iframe });
-            iframes.splice(i, 1);
-        }
-    }
-}
-    
 iframes.forEach(iframe => {
     if (vw >= vh) {
         iframe.width = vw / 2;
