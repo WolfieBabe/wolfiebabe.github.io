63fc2ee20e566d22eb5aff454c7eed8683c1d4cb
WolfieBabe <110204635+WolfieBabe@users.noreply.github.com>
2025-07-22 12:48:45 -0500

    Update iframeHandler.js

--- iframeHandler.js
+++ iframeHandler.js
@@ -1,5 +1,7 @@
 let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+var modW
+var modH
 
 if (vw >= vh) {
     modW = vw / 2;
