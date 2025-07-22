2c6388977231524200709a52698becce36e31949
WolfieBabe <110204635+WolfieBabe@users.noreply.github.com>
2025-07-22 12:30:41 -0500

    Update iframeHandler.js

--- iframeHandler.js
+++ iframeHandler.js
@@ -1,14 +1,22 @@
 let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
+if (vw >= vh) {
+    modW = vw / 2;
+    modH = vh * .8;
+} else {
+    modW = vw * .8;
+    modH = vh / 2;
+}
+
 const iframes = Array.from(document.querySelectorAll('iframe')).concat(Array.from(document.querySelectorAll('.pdfobject-container'))).filter(element => !Array.from(document.querySelectorAll('iframe.pdfobject')).includes(element));
 
-iframes.forEach(iframe => {
-    if (vw >= vh) {
-        iframe.width = vw / 2;
-        iframe.height = vh * .8;   
+iframes.forEach(element => {
+    if (element.tagName === 'IFRAME') {
+        element.width = Math.round(modW);
+        element.height = Math.round(modH);
     } else {
-        iframe.height = vw / 2;
-        iframe.width = vh * .8;
+        element.style.width = Math.round(modW) + 'px';
+        element.style.height = Math.round(modH) + 'px';
     }
 })
