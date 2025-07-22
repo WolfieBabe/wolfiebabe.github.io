8509c5512ca9540ab2c1d17879e39157e41313d8
WolfieBabe <110204635+WolfieBabe@users.noreply.github.com>
2025-07-22 12:02:56 -0500

    Update iframeHandler.js

--- iframeHandler.js
+++ iframeHandler.js
@@ -1,7 +1,7 @@
 let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
-const iframes = Array.from(document.querySelectorAll('iframe', '.pdfobject-container'));
+const iframes = Array.from(document.querySelectorAll('iframe')).concat(Array.from(document.querySelectorAll('.pdfobject-container'))).filter(element => !Array.from(document.querySelectorAll('iframe.pdfobject')).includes(element));
 
 iframes.forEach(iframe => {
     if (vw >= vh) {
