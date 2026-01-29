056a4429fb7e032ce5cd853dbe31563b8070f6f6
WolfieBabe <110204635+WolfieBabe@users.noreply.github.com>
2026-01-29 02:51:25 +0000

    Adjust iframe and image dimensions dynamically
    Refactor iframe and image handling based on viewport dimensions.

--- iframeHandler.js
+++ iframeHandler.js
@@ -2,8 +2,10 @@ let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth |
 let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 var modW
 var modH
+var imgW
+var imgH
 
-if (vw >= vh) {
+if (vw >= vh * .8) {
     modW = vw / 2;
     modH = vh * .8;
 } else {
@@ -14,14 +16,20 @@ if (vw >= vh) {
 const iframes = Array.from(document.querySelectorAll('iframe')).concat(Array.from(document.querySelectorAll('.pdfobject-container'))).filter(element => !Array.from(document.querySelectorAll('iframe.pdfobject')).includes(element));
 const imgs = Array.from(document.querySelectorAll('img'))
 
-const alters = iframes.concat(imgs)
-
-alters.forEach(element => {
+iframes.forEach(element => {
     if (element.tagName === 'IFRAME') {
         element.width = Math.round(modW);
         element.height = Math.round(modH);
     } else {
         element.style.width = Math.round(modW) + 'px';
         element.style.height = Math.round(modH) + 'px';
+    }
+})
+
+imgs.forEach(element => {
+    if (vw >= vh) {
+        imgH = vh / 2;
+    } else {
+        imgW = vw * .8;
     }
 })
