ffd9bed4edef834f65af4d9a3fb5a0f7e43bc848
WolfieBabe <110204635+WolfieBabe@users.noreply.github.com>
2026-01-29 18:47:39 +0000

    Refactor iframe and image handling in iframeHandler.js
    Refactor iframe and image handling logic into a separate function and update variable declarations.

--- iframeHandler.js
+++ iframeHandler.js
@@ -1,9 +1,9 @@
 let avw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 let avh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-var modW
-var modH
-var imgW
-var imgH
+var modW;
+var modH;
+var iframes;
+var imgs;
 
 if (avw >= aavh * .8) {
     modW = avw / 2;
@@ -11,25 +11,30 @@ if (avw >= aavh * .8) {
 } else {
     modW = avw * .8;
     modH = avh / 2;
-}
+};
 
-const iframes = Array.from(document.querySelectorAll('iframe')).concat(Array.from(document.querySelectorAll('.pdfobject-container'))).filter(element => !Array.from(document.querySelectorAll('iframe.pdfobject')).includes(element));
-const imgs = Array.from(document.querySelectorAll('img'))
+function run_handling() {
 
-iframes.forEach(element => {
-    if (element.tagName === 'IFRAME') {
-        element.width = Math.round(modW);
-        element.height = Math.round(modH);
-    } else {
-        element.style.width = Math.round(modW) + 'px';
-        element.style.height = Math.round(modH) + 'px';
-    }
-})
+    iframes = Array.from(document.querySelectorAll('iframe')).concat(Array.from(document.querySelectorAll('.pdfobject-container'))).filter(element => !Array.from(document.querySelectorAll('iframe.pdfobject')).includes(element));
+    imgs = Array.from(document.querySelectorAll('img'));
+    
+    iframes.forEach(element => {
+        if (element.tagName === 'IFRAME') {
+            element.width = Math.round(modW);
+            element.height = Math.round(modH);
+        } else {
+            element.style.width = Math.round(modW) + 'px';
+            element.style.height = Math.round(modH) + 'px';
+        }
+    })
+    
+    imgs.forEach(element => {
+        if (avw >= avh) {
+            element.style.height = "50vh";
+        } else {
+            element.style.width = "80vw";
+        }
+    })
+};
 
-imgs.forEach(element => {
-    if (avw >= avh) {
-        element.style.height = "50vh";
-    } else {
-        element.style.width = "80vh";
-    }
-})
+document.addEventListener("load",func);
