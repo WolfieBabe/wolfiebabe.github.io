9cb7e5f48c70aaf424aadd8a6b9ba240bb8dce43
WolfieBabe <110204635+WolfieBabe@users.noreply.github.com>
2026-01-29 20:17:19 +0000

    Refactor run_handling function for clarity

--- iframeHandler.js
+++ iframeHandler.js
@@ -13,28 +13,23 @@ if (avw >= avh * .8) {
     modH = avh / 2;
 };
 
-function run_handling() {
-
-    iframes = Array.from(document.querySelectorAll('iframe')).concat(Array.from(document.querySelectorAll('.pdfobject-container'))).filter(element => !Array.from(document.querySelectorAll('iframe.pdfobject')).includes(element));
-    imgs = document.images;
+iframes = Array.from(document.querySelectorAll('iframe')).concat(Array.from(document.querySelectorAll('.pdfobject-container'))).filter(element => !Array.from(document.querySelectorAll('iframe.pdfobject')).includes(element));
+imgs = document.images;
     
-    iframes.forEach(element => {
-        if (element.tagName === 'IFRAME') {
-            element.width = Math.round(modW);
-            element.height = Math.round(modH);
-        } else {
-            element.style.width = Math.round(modW) + 'px';
-            element.style.height = Math.round(modH) + 'px';
-        }
-    })
+iframes.forEach(element => {
+    if (element.tagName === 'IFRAME') {
+        element.width = Math.round(modW);
+        element.height = Math.round(modH);
+    } else {
+        element.style.width = Math.round(modW) + 'px';
+        element.style.height = Math.round(modH) + 'px';
+    }
+});
     
-    for (let i = 0; i < imgs.length; i++) {
-        if (avw >= avh) {
-            imgs[i].style.height = "50vh";
-        } else {
-            imgs[i].style.width = "80vw";
-        }
-    })
-};
-
-document.addEventListener("load",run_handling());
+for (let i = 0; i < imgs.length; i++) {
+    if (avw >= avh) {
+        imgs[i].style.height = "50vh";
+    } else {
+        imgs[i].style.width = "80vw";
+    }
+});
